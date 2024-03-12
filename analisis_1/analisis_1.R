80c0f6e3496642aed5d1f2a87aef91164dd3264c
Mariana <mariana.yuste.rz@gmail.com>
2024-03-12 16:09:14 -0600

    perf: :zap: agregando paralelización
    pero no ayudó mucho y en redes grandes no funcionó :(

--- analisis_1/analisis_1.R
+++ analisis_1/analisis_1.R
@@ -2,17 +2,33 @@
 #   Eliminar una por una las regulaciones de cada nodo
 #   Recuperar attrs y vasijas
 library(BoolNet)
+library(foreach)
+library(doMC)
+registerDoMC(2) #my mac has 2 cores
 setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis/analisis_1')
 
-net <- loadNetwork('original.txt')
-#attr <- getAttractors(net)
-#plotAttractors(attr)
+### Probar paralelización del código
+setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis/test_parallel/')
+start_time <- Sys.time()
+for(c in 0:500){ #Total time: 6.6893789768219. Slow but gives each result as soon as is created
+  #foreach(c=0:500) %dopar% { #Total time: 5.35348200798035. Faster but gives all the results at the end
+  filename <- '0.txt'
+  net <- loadNetwork(filename)
+  attr_prueba <- getAttractors(net,method='sat.exhaustive')
+  nombre <- paste('attr_',c,'.txt',sep='')
+  l <- c()
+  for (a in attr_prueba[2]){for (b in a){l <- c(l,b[1])}} #guardar solo los atractores
+  write.table(l,nombre,row.names = F,col.names = F)
+  print(c)
+}
+end_time <- Sys.time()
+print(paste('Total time:',end_time-start_time))
 
-netprueba <- loadNetwork( '2.txt')
-attr_prueba <- getAttractors(netprueba)
-attr_prueba
 
-for (c in 1:59) {
+### Correr las redes con cada modificación (en documentos .txt hechos a mano)
+foreach(c=62:65) %dopar% {
+  #for(c in 61:117){
+  start <- Sys.time()
   filename <- paste(c,'.txt',sep='')
   net <- loadNetwork(filename)
   attr_prueba <- getAttractors(net,method='sat.exhaustive')
@@ -20,8 +36,13 @@ for (c in 1:59) {
   l <- c()
   for (a in attr_prueba[2]){for (b in a){l <- c(l,b[1])}} #guardar solo los atractores
   write.table(l,nombre,row.names = F,col.names = F)
+  end <- Sys.time()
+  print(paste('Finished with ',c,' - time taked: ',end-start))
 }
 
+net <- loadNetwork('original.txt')
+#attr <- getAttractors(net)
+#plotAttractors(attr)
 
 #obtener total de regulaciones:
 total_int <- 0
