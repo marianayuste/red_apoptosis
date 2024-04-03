ad7afef163487e91944682a47f22d0dd4f86b0e5
Mariana <mariana.yuste.rz@gmail.com>
2024-04-02 19:57:45 -0600

    feat: :sparkles: analisis_2.R listo
    analisis_2.R listo para generar los attrs de cada perturbación bitflip guardada por bitflip.ipynb

--- analisis_2/analisis_2.R
+++ analisis_2/analisis_2.R
@@ -1,22 +1,24 @@
 library(BoolNet)
 setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis/analisis_2/perturbaciones')
 
-# NODOS Y TOTAL DE SUS PERTURBACIONES----------
 nodes = c('ICL','FAcore','FANCD2I','Nuc1','RNF4','Nuc2','ADD','DSB','TLS','FAhrr','HRR2','NHEJ','TNFa','p38MAPK','ATR','ATM','p530','MDM2','p53a0','p21','p53aRE','p53k0','p53kRE','CASP3','IAP','Wip1','CDK1_AurA','Plk1','CDC25','CycB_CDK1')
-total_per = c(3, 6, 3, 3, 2, 8, 4, 10, 2, 13, 5, 12, 5, 2, 6, 7, 6, 3, 6, 2, 4, 35, 5, 3, 1, 2, 6, 8, 7, 6)
 
 #CARGAR CADA RED PERTURBADA Y OBTENER ATRACTORES
 #las redes perturbadas elegidas se generaron en python documento bitflip.ipynb
-data <- read.csv('elegidas.txt', header = F)
+data <- read.csv('elegidas.txt', header = F,sep = ';')
+elegidas <- data[-length(data)]
 
-for (n in 1:length(nodes)){ #para cada nodo
-  for (p in 0:(total_per[n]-1)){ #para cada perturbacion
-    archivo <- paste(nodes[n],'_',p,'.txt', sep='')
-    net <- loadNetwork(archivo)
-    attr <- getAttractors(net,method='sat.exhaustive') #sat.exhaustive solo da attrs, no indica vasijas
-    nombre <- paste(nodes[n],'_',p,'_attrs.txt', sep='')
-    l = c()
-    for (a in attr[2]){for (b in a){l <- c(l,b[1])}} #guardar solo los atractores
-    write.table(l,nombre,row.names = F,col.names = F)
-  }
+#OBTENER LOS ATTRS DE CADA PERTURBACION
+for (p in elegidas){ #para cada perturbación
+  nodo <- strsplit(p, split=',')[1] #nodo de la perturbación
+  per <- strsplit(p, split=',')[2] #numero de perturbación
+  archivo <- paste(nodo,'_',per,'.txt',sep='')
+  net <- loadNetwork(archivo)
+  attr <- getAttractors(net,method='sat.exhaustive') #sat.exhaustive solo da attrs, no indica vasijas
+  nombre <- paste(nodo,'_',per,'_attrs.txt', sep='')
+  l = c()
+  for (a in attr[2]){for (b in a){l <- c(l,b[1])}} #guardar solo los atractores
+  write.table(l,nombre,row.names = F,col.names = F)
+  rm(list = ls())
 }
+
