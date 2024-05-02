7a27c22c9d2a2880232592a7229513127a6c7b56
Mariana <mariana.yuste.rz@gmail.com>
2024-05-02 15:35:17 -0600

    mejorar README con analisis_3, realizar analisis_3 desde ICL constitutivamente activo

--- analisis_3/analisis_3.R
+++ analisis_3/analisis_3.R
@@ -1,12 +1,12 @@
 library(BoolNet)
 library("rlist")
-setwd('/Users/tbmovies/Downloads/red_apoptosis-main')
+setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis')
 
 ### GUARDAR LA RED ORIGINAL----------------------
 net <- loadNetwork('original.txt')
 
 
-setwd('/Users/tbmovies/Downloads/red_apoptosis-main/analisis_3')
+setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis/analisis_3')
 
 ### CORRER ATRACTORES ---------------------------
 start = Sys.time()
@@ -16,15 +16,19 @@ ini_total = 1000000
 #10,000 tarda 3 segs
 #100,000 tarda 34 segs
 #1,000,000 tarda 
-attr <- getAttractors(net,
+
+#crear red con ICL siempre en 1:
+net_ICL <- fixGenes(net,1,1)
+
+attr <- getAttractors(net_ICL,
                       type="asynchronous",
-                      method="random",
-                      startStates=ini_total) #10,000,000 son 1/5 aprox del total de edos iniciales posibles
+                      method="random",  #1e+03 #10,000,000 son 1/5 aprox del total de edos iniciales posibles
+                      startStates=ini_total)
 end <- Sys.time()
 print(paste('duracion: ',start,'-',end,'=',end-start))
 
 #guardar los attrs obtenidos
-nombre <- paste('asyn_attrs_',toString(ini_total),'.txt', sep='')
+nombre <- paste('asyn_attrs_ICL_',toString(ini_total),'.txt', sep='')
 l = c()
 for (a in attr[2]){for (b in a){l <- c(l,b[1])}} #guardar solo los atractores
 write.table(l,nombre,row.names = F,col.names = F)
