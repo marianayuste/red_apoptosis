7a1d09632aaed7baf7c975bd2f4027774f8289da
Mariana <mariana.yuste.rz@gmail.com>
2024-05-02 15:38:09 -0600

    Revert "mejorar README con analisis_3, realizar analisis_3 desde ICL constitutivamente activo"
    This reverts commit 7a27c22c9d2a2880232592a7229513127a6c7b56.

--- README.txt
+++ README.txt
@@ -3,14 +3,10 @@ Qué se hizo y en qué documentos:
 Análisis 1. Perturbaciones quitando una por una las regulaciones de cada nodo 
     - Se hicieron las perturbaciones a mano, guardando cada una en un archivo distinto como 'n.txt' donde n es el número de perturbación, todo se guardó en carpeta analisis_1
     - Se obtuvieron los attrs de cada red perturbada en analisis_1.R, que también se encuentra en dicha carpeta. Se guardaron todos los attrs como números en decimal en documento 'attr_n.txt'
-    - Se leyeron los resultados y total de perturbaciones con cambios en analisis_1.ipynb
+    - Se leyeron los resultados y total de perturbaciones con cambios en 'analisis_1.ipynb'
     
     
 Análisis 2. Perturbaciones bitflip a la red original
     - Se crearon las redes perturbadas en bitflip.ipynb
     - Se corrieron 150 de ellas (elegidas al azar) en bitflip.R
-    - Se leyeron los resultados y total de perturbaciones con cambios en bitflip.ipynb
-
-Análisis 3. Análisis asíncrono de la red
-    - En analisis_3.R: Se corrió la red original.txt de forma asíncrona, desde un millón de estados iniciales elegidos aleatoriamente, y se guardaron los atractores obtenidos
-    - Se leyeron los resultados en analisis_3.ipynb
+    - Se leyeron los resultados y total de perturbaciones con cambios en 'bitflip.ipynb'
--- analisis_3/analisis_3.R
+++ analisis_3/analisis_3.R
@@ -1,12 +1,12 @@
 library(BoolNet)
 library("rlist")
-setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis')
+setwd('/Users/tbmovies/Downloads/red_apoptosis-main')
 
 ### GUARDAR LA RED ORIGINAL----------------------
 net <- loadNetwork('original.txt')
 
 
-setwd('/Users/mariana/Dropbox/redes_alfredo/red_apoptosis/analisis_3')
+setwd('/Users/tbmovies/Downloads/red_apoptosis-main/analisis_3')
 
 ### CORRER ATRACTORES ---------------------------
 start = Sys.time()
@@ -16,19 +16,15 @@ ini_total = 1000000
 #10,000 tarda 3 segs
 #100,000 tarda 34 segs
 #1,000,000 tarda 
-
-#crear red con ICL siempre en 1:
-net_ICL <- fixGenes(net,1,1)
-
-attr <- getAttractors(net_ICL,
+attr <- getAttractors(net,
                       type="asynchronous",
-                      method="random",  #1e+03 #10,000,000 son 1/5 aprox del total de edos iniciales posibles
-                      startStates=ini_total)
+                      method="random",
+                      startStates=ini_total) #10,000,000 son 1/5 aprox del total de edos iniciales posibles
 end <- Sys.time()
 print(paste('duracion: ',start,'-',end,'=',end-start))
 
 #guardar los attrs obtenidos
-nombre <- paste('asyn_attrs_ICL_',toString(ini_total),'.txt', sep='')
+nombre <- paste('asyn_attrs_',toString(ini_total),'.txt', sep='')
 l = c()
 for (a in attr[2]){for (b in a){l <- c(l,b[1])}} #guardar solo los atractores
 write.table(l,nombre,row.names = F,col.names = F)
